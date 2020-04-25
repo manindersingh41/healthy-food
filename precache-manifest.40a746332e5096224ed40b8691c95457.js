f74f10047d5095e123c47cd33f653498f5436292
manindersingh41 <monu642858@gmail.com>
2020-04-26 00:06:44 +0530

    Updates

---
+++ precache-manifest.40a746332e5096224ed40b8691c95457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded8a52e2750b36fa2f1bc9525e400bf",
+    "url": "/healthy-food/index.html"
+  },
+  {
+    "revision": "3ee171afd3b99977846c",
+    "url": "/healthy-food/static/css/main.3ddbdafb.chunk.css"
+  },
+  {
+    "revision": "4f6368d350e0f471be23",
+    "url": "/healthy-food/static/js/2.55f9f4ec.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/healthy-food/static/js/2.55f9f4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee171afd3b99977846c",
+    "url": "/healthy-food/static/js/main.cdccbc15.chunk.js"
+  },
+  {
+    "revision": "9c59eebc1fcf894fe992",
+    "url": "/healthy-food/static/js/runtime-main.10e236bb.js"
+  }
+]);
